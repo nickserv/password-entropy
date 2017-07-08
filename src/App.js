14466465768e2c17e32b4f1e480598422c7fa9d7
Nick McCurdy <nick@nickmccurdy.com>
2017-07-08 15:19:12 -0400

    Static prototype

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+function bits (size) {
+  return Math.floor(Math.log2(size)) + 1
 }
 
-export default App;
+const App = props => {
+  const listSize = 7776
+  const passwordSize = 6
+  const size = listSize ** passwordSize
+  return <div>
+    <p>{size} passwords</p>
+    <p>{bits(size)} bits of entropy</p>
+  </div>
+}
+
+export default App
