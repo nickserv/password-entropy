f6e9f8be9fc79399e52b84b76ae85ec0c1bae5e9
Nick McCurdy <nick@nickmccurdy.com>
2017-07-09 01:30:21 -0400

    Basic input support

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function bits (size) {
-  return Math.floor(Math.log2(size)) + 1
+export default class App extends Component {
+  state = {
+    listSize: 7776,
+    passwordSize: 6
+  }
+
+  static bits (size) {
+    return Math.floor(Math.log2(size)) + 1
+  }
+
+  numberSetter (key) {
+    return (event) => this.setState({ [key]: parseInt(event.target.value, 10) })
+  }
+
+  render () {
+    const size = this.state.listSize ** this.state.passwordSize
+    return <div>
+      <h1>Password Entropy</h1>
+      <h2>Diceware</h2>
+      <p>
+        <input value={this.state.passwordSize} onChange={this.numberSetter('passwordSize')} type="number" min="1" required/>
+        words from a set of
+        <input value={this.state.listSize} onChange={this.numberSetter('listSize')} type="number" min="1" required/>
+      </p>
+      <p>Roughly {size.toPrecision(4)} passwords ({this.constructor.bits(size)} bits of entropy)</p>
+    </div>
+  }
 }
-
-const App = props => {
-  const listSize = 7776
-  const passwordSize = 6
-  const size = listSize ** passwordSize
-  return <div>
-    <p>{size} passwords</p>
-    <p>{bits(size)} bits of entropy</p>
-  </div>
-}
-
-export default App
