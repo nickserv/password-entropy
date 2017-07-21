e4d93c1b8bad20676b077561d789616c66e2dc93
Nick McCurdy <nick@nickmccurdy.com>
2017-07-21 18:51:32 -0400

    Simplify PasswordEntropy tests by only using shallow renderer

--- src/PasswordEntropy.test.js
+++ src/PasswordEntropy.test.js
@@ -1,34 +1,21 @@
-import change from './change'
-import { mount, shallow } from 'enzyme'
+import { shallow } from 'enzyme'
 import PasswordEntropy from './PasswordEntropy'
 import React from 'react'
 
-const node = (<PasswordEntropy/>)
+function getWrapper (possibleItems) {
+  const wrapper = shallow(<PasswordEntropy/>)
+  wrapper.instance().handlePossibleItemsChange(possibleItems)
+  return wrapper.update()
+}
 
-it('renders without valid PossibleItems', () => {
-  expect(shallow(node)).toMatchSnapshot()
+it('renders without valid possibleItems', () => {
+  expect(getWrapper(0)).toMatchSnapshot()
 })
 
-it('renders with a smaller result', () => {
-  const wrapper = mount(node)
-  change(wrapper.find({ name: 'length' }), { value: 1 })
-  expect(wrapper).toMatchSnapshot()
+it('renders with few possibleItems', () => {
+  expect(getWrapper(10)).toMatchSnapshot()
 })
 
-it('renders with a larger result', () => {
-  const wrapper = mount(node)
-  wrapper.instance().handlePossibleItemsChange(7776)
-  expect(wrapper).toMatchSnapshot()
-})
-
-it('recalculates when length changes', () => {
-  const wrapper = mount(node)
-  change(wrapper.find({ name: 'length' }), { value: 7 })
-  expect(wrapper).toMatchSnapshot()
-})
-
-it('recalculates when options change', () => {
-  const wrapper = mount(node)
-  wrapper.instance().handlePossibleItemsChange(10)
-  expect(wrapper).toMatchSnapshot()
+it('renders with many possibleItems', () => {
+  expect(getWrapper(7776)).toMatchSnapshot()
 })
