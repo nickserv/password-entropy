6230c1c6d80bad3ad5c616685a047b11049ba9a4
Nick McCurdy <nick@nickmccurdy.com>
2017-07-23 21:13:42 -0400

    Remove service worker (no HTTPS in production)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import PasswordEntropy from './PasswordEntropy';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<PasswordEntropy />, document.getElementById('root'));
-registerServiceWorker();
