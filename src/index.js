83413321a582b05d9ae1cab2a28b3027f2cb927f
Nick McCurdy <nick@nickmccurdy.com>
2017-07-25 06:23:29 -0400

    Remove custom styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css'
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import PasswordEntropy from './PasswordEntropy';
 
 ReactDOM.render(<PasswordEntropy />, document.getElementById('root'));
