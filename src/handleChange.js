67dca0c82489044b4f0ca297ddd2234dcb48a034
Nick McCurdy <nick@nickmccurdy.com>
2017-07-25 21:13:00 -0400

    Use destructuring in getValue

--- src/handleChange.js
+++ src/handleChange.js
@@ -1,8 +1,8 @@
-function getValue (target) {
-  switch (target.type) {
-    case 'checkbox': return target.checked
-    case 'number': return parseInt(target.value, 10)
-    default: return target.value
+function getValue ({ checked, type, value }) {
+  switch (type) {
+    case 'checkbox': return checked
+    case 'number': return parseInt(value, 10)
+    default: return value
   }
 }
 
