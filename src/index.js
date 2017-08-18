b0266a224ef726f2cfd5affcdcc6aae01c61eca7
Nick McCurdy <nick@nickmccurdy.com>
2017-08-18 12:49:41 -0400

    Use redux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux'
 import reducers from './reducers'
 import { createStore } from 'redux'
+import { devToolsEnhancer } from 'redux-devtools-extension'
 import 'bootstrap/dist/css/bootstrap.css'
 
 render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={createStore(reducers, devToolsEnhancer())}>
     <PasswordEntropy/>
   </Provider>,
   document.getElementById('root')
