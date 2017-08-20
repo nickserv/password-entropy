cb1eedba03475218f07d0e5fa321ec1536c6ddb7
Nick McCurdy <nick@nickmccurdy.com>
2017-08-20 08:43:46 -0400

    Replace validator option with min

--- src/reducers.js
+++ src/reducers.js
@@ -2,22 +2,18 @@ import GenericOptions from './options/GenericOptions'
 import { assoc, identity, map, T } from 'ramda'
 import { combineReducers } from 'redux'
 
-function createReducer(actionType, initialState, { callback = identity, validator } = {}) {
+function createReducer(actionType, initialState, { callback = identity, min } = {}) {
   return (state = initialState, { payload, type }) => {
     const matchesAction = type === actionType
-    const isValid = !validator || validator(payload)
+    const isValid = !Number.isInteger(min) || payload >= min
 
     return matchesAction && isValid ? callback(payload, state) : state
   }
 }
 
-const length = createReducer('SET_LENGTH', 6, {
-  validator: n => n >= 1
-})
+const length = createReducer('SET_LENGTH', 6, { min: 1 })
 
-const custom = createReducer('SET_CUSTOM', 1, {
-  validator: n => n >= 0
-})
+const custom = createReducer('SET_CUSTOM', 1, { min: 0 })
 
 const generic = createReducer('TOGGLE_GENERIC', map(T, GenericOptions.toggles), {
   callback: ({ checked, name }, state) => assoc(name, checked, state)
