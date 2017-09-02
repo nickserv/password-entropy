d59a9969cb6d65b398601514f1ca6ff1db64580d
Nick McCurdy <nick@nickmccurdy.com>
2017-09-02 05:33:56 -0400

    Set up material-ui

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
+import { MuiThemeProvider } from 'material-ui/styles'
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import { devToolsEnhancer } from 'redux-devtools-extension'
-import 'bootstrap/dist/css/bootstrap.css'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
 
 import PasswordEntropy from './app/PasswordEntropy'
 import reducers from './reducers'
+import './index.css'
 
 render(
   <Provider store={createStore(reducers, devToolsEnhancer())}>
-    <PasswordEntropy />
+    <MuiThemeProvider>
+      <PasswordEntropy />
+    </MuiThemeProvider>
   </Provider>,
   document.getElementById('root')
 )
