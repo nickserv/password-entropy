fc51d2c63ab2cfc463a7e79619c1637ad8d974b9
Nick McCurdy <nick@nickmccurdy.com>
2017-09-28 03:12:28 -0400

    Update to React 16 (fix #22)

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 import 'jest-enzyme'
+
+Enzyme.configure({ adapter: new Adapter() })
