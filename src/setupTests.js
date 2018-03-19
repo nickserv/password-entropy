cb9822be4e3213410514db97e340f2cd70b5116a
Nick McCurdy <nick@nickmccurdy.com>
2018-03-19 03:59:07 -0400

    Replace jest-enzyme with just enzyme-to-json for simplicity

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,4 @@
-import Enzyme from 'enzyme'
+import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import 'jest-enzyme'
 
-Enzyme.configure({ adapter: new Adapter() })
+configure({ adapter: new Adapter() })
