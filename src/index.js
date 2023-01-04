f91b88f6dc249334137b6b1ef71a9f90b84f5ed0
Nick McCurdy <nick@nickmccurdy.com>
2023-01-04 09:30:34 -0500

    Use strict mode

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.css'
-import React from 'react'
+import React, { StrictMode } from 'react'
 import { render } from 'react-dom'
 import PasswordEntropyContainer from './components/PasswordEntropyContainer'
 import registerServiceWorker from './registerServiceWorker'
 
-render(<PasswordEntropyContainer />, document.getElementById('root'))
+render(
+  <StrictMode>
+    <PasswordEntropyContainer />
+  </StrictMode>,
+  document.getElementById('root')
+)
 registerServiceWorker()
 if (import.meta.webpackHot) import.meta.webpackHot.accept()
